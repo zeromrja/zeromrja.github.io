b456f05e2b1d409ae78aeedd3e5617183e5c6db1
zeromrja <129565385+zeromrja@users.noreply.github.com>
2024-07-13 18:39:02 +0200

    Update idioma.js

--- SkinPackDoraemon/idioma.js
+++ SkinPackDoraemon/idioma.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Load selected language or default to Spanish
-    const savedLanguage = localStorage.getItem('selectedLanguage') || 'es';
+    const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
     languageSelect.value = savedLanguage;
     loadTranslations(savedLanguage);
 });
